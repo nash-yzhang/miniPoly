ffed17be887c9b18fd206088a1ee3bfdec806911
Yue Zhang (Nash) <yue.zhang7@outlook.com>
2019-12-12 10:54:00 +0100

    Initial commit

---
+++ miniPoly_play.py
@@ -0,0 +1,11 @@
+import numpy
+from multiprocessing import Process,Queue
+from glumpy import app, gl, glm
+import time
+def p1(q):
+
+    q.put(time.time())g
+
+q = Queue()
+p = Process(target = p1, args = (q,))
+print(q.get())
