6ef941df890e6218346554d4fbbe36aa9d7e4644
Yue Zhang (Nash) <yue.zhang7@outlook.com>
2019-12-23 22:58:51 +0100

    Bugger decorator and _bugger class in Decoration.py now are functioning.

--- Decoration.py
+++ Decoration.py
@@ -1,26 +1,33 @@
 import multiprocessing as mp
-from queue import Queue
-from functools import update_wrapper,wraps
-from IPython import embed
+from functools import wraps
+# from IPython import embed
+# noinspection SpellCheckingInspection
 
-class _bugger(object):
+class _bugger (object) :
+    _func = None
+
     def __init__(self, func, varname):
         self.varname = varname
-        self.func    = func
-        self.varbuffer = []
-        self.__module__ = "__main__"
+        self._func = func
+        self.varbuffer = mp.Queue()
+        self.Process = mp.Process (target=self._func, args=(self,))
 
-    def __call__(self,*args,**kwargs):
-        return self.func(self,*args,**kwargs)
+    def append(self, localvar, varname=None) :
+        if not varname :
+            varname = self.varname
+        tout = {}
+        for key, value in localvar.items () :
+            if key in varname :
+                tout[key] = value
+        self.varbuffer.put (tout)
 
-    def append(self,localvar):
-        self.varbuffer.insert(0,{key: value for key,value in localvar.items() if key in self.varname})
 
-def Bugger(varname,function = None):
-    if function:
-        return _bugger(function,varname)
-    else:
-        @wraps(function)
-        def wrapper(function,**kwargs):
-            return _bugger(function,varname,**kwargs)
+def Bugger(varname, func=None) :
+    if func :
+        return _bugger (func, varname)
+    else :
+        @wraps (func)
+        def wrapper(func, **kwargs) :
+            return _bugger (func, varname)
+
         return wrapper
