60450599db104b0a8f49ac56e52e6b5b351ca0a0
Yue Zhang (Nash) <yue.zhang7@outlook.com>
2020-07-07 16:51:54 +0200

    fragrenderer now support adding texture

--- stimulus/fragrenderer.py
+++ stimulus/fragrenderer.py
@@ -2,6 +2,7 @@ from scipy.ndimage import gaussian_filter
 from bin.glarage import *
 from glumpy import gl, gloo
 import imgui, os, sys
+from PIL import Image
 
 self = None
 
@@ -17,12 +18,15 @@ def prepare():
     }
     """
     self._shader_folder = 'stimulus/shaderfile/'
+    self._texture_folder = 'stimulus/shaderfile/'
     self._frag_shader_fn = None
+    self._texture_fn = None
     self._frag = None
     self._frag_render_program = None
     self._default_param_val = {'float':1.,'vec2': (1.,1.),'u_time':0.,'u_resolution':(512,512),
-                               'position':[[-1,-1],[1,-1],[-1,1],[1,1]]}
+                               'position':[[-1,-1],[1,-1],[-1,1],[1,1]],'texture':np.ones([10,10])}
     self.frag_fn_idx = 0
+    self.texture_fn_idx = 0
     self.load_shader_win = False
     self._poped = False
     self._setpoped = False
@@ -59,10 +63,13 @@ def set_widgets():
                         self._frag_render_program[u[0]] = self._default_param_val['float']
                     elif u[1] == gl.GL_FLOAT_VEC2:
                         self._frag_render_program[u[0]] = self._default_param_val['vec2']
+                    elif u[1] == gl.GL_SAMPLER_2D:
+                        self._frag_render_program[u[0]] = self._default_param_val['texture']
                     if u[0] == 'u_time':
                         self._frag_render_program[u[0]] = self._default_param_val['u_time']
                     elif u[0] == 'u_resolution':
                         self._frag_render_program[u[0]] = self._default_param_val['u_resolution']
+
                 self._frag_render_program['position'] = self._default_param_val['position']
                 self._refresh_on = True
                 self.load_shader_win = False
@@ -82,6 +89,23 @@ def set_widgets():
                     _,self._frag_render_program[u[0]] = imgui.drag_float(u[0],self._frag_render_program[u[0]],self._changespeed)
                 elif u[1] == gl.GL_FLOAT_VEC2:
                     _,self._frag_render_program[u[0]] = imgui.drag_float2(u[0], *self._frag_render_program[u[0]],self._changespeed)
+                elif u[1] == gl.GL_SAMPLER_2D:
+                        _, self._texture_folder = imgui.input_text(' ', self._texture_folder, 256)
+                        if os.path.isdir(self._texture_folder):
+                            file_list = [file for file in os.listdir(self._shader_folder) if file.endswith((".png",
+                                                                                                            ".jpg",
+                                                                                                            "jpeg",
+                                                                                                            ".bmp"))]
+                        else:
+                            file_list = []
+                        _, self.texture_fn_idx = imgui.combo(
+                            "", self.texture_fn_idx, file_list
+                        )
+                        if imgui.button("Select"):
+                            sys.path.insert(0, self._texture_folder)
+                            self._texture_fn = file_list[self.texture_fn_idx]
+                            temp = np.array(Image.open(self._texture_folder+self._texture_fn))/256
+                            self._frag_render_program[u[0]] = temp.astype(np.float32).view(gloo.TextureFloat2D)
         imgui.end()
 
         self.pop_check()
