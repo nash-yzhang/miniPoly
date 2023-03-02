8e5d35ecf5ec6b6f65deab79799d009f127a5994
Yue Zhang (Nash) <yue.zhang@bi.mpg.de>
2023-03-02 11:47:42 +0100

    integrating; somehow drag&drop doesn't work anymore

--- apps/TISCam_compiler/main.py
+++ apps/TISCam_compiler/main.py
@@ -1,15 +1,23 @@
 from apps.TISCam_compiler.app_bin import CameraInterface
-from bin.compiler import TISCameraDriver
+from apps.servo_compiler.app_bin import ServoCompilerGUI
+from bin.compiler import TISCameraDriver, PololuServoDriver,SerialCommander
 from bin.minion import LoggerMinion
 from time import sleep
 
 if __name__ == '__main__':
     Cam = TISCameraDriver('Tiscam_1', refresh_interval=10)
     GUI = CameraInterface('GUI', refresh_interval=5)
+    pololu_servo = PololuServoDriver('Pololu', refresh_interval=1, port_name='COM9',
+                                     servo_dict={'yaw': 3, 'radius': 5, 'flagging': 1, })
+    pololu_servo.connect(GUI)
+    arduino_board = SerialCommander('Arduino', refresh_interval=1, port_name='COM10')
+    arduino_board.connect(GUI)
     logger = LoggerMinion('TestCam logger')
     Cam.connect(GUI)
     Cam.attach_logger(logger)
     GUI.attach_logger(logger)
+    pololu_servo.attach_logger(logger)
+    arduino_board.attach_logger(logger)
     logger.run()
     Cam.run()
     sleep(2)
--- apps/servo_compiler/pololu_test.py
+++ apps/servo_compiler/pololu_test.py
@@ -4,18 +4,18 @@ from apps.servo_compiler.app_bin import ServoCompilerGUI
 
 if __name__ == '__main__':
     GUI = ServoCompilerGUI('GUI', refresh_interval=1)
-    pololu_servo = PololuServoDriver('Pololu', refresh_interval=1, port_name='COM6',
-                                     servo_dict={'yaw': 3, 'radius': 5, 'flagging': 1, })
-    pololu_servo.connect(GUI)
-    arduino_board = SerialCommander('Arduino', refresh_interval=1, port_name='COM3')
+    # pololu_servo = PololuServoDriver('Pololu', refresh_interval=1, port_name='COM6',
+    #                                  servo_dict={'yaw': 3, 'radius': 5, 'flagging': 1, })
+    # pololu_servo.connect(GUI)
+    # arduino_board = SerialCommander('Arduino', refresh_interval=1, port_name='COM7')
     # arduino_board = ArduinoDriver('Arduino', interval=1, port_name='COM3',
     #                               pin_address={'LED1': 'd:8:o'})
-    arduino_board.connect(GUI)
+    # arduino_board.connect(GUI)
     logger = LoggerMinion('Logger')
     GUI.attach_logger(logger)
-    pololu_servo.attach_logger(logger)
-    arduino_board.attach_logger(logger)
+    # pololu_servo.attach_logger(logger)
+    # arduino_board.attach_logger(logger)
     logger.run()
-    arduino_board.run()
-    pololu_servo.run()
+    # arduino_board.run()
+    # pololu_servo.run()
     GUI.run()
