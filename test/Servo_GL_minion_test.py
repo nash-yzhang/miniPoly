769ef169c5be0f1065ff54fc11cdd8317fda4b05
Yue Zhang (Nash) <yue.zhang@bi.mpg.de>
2023-07-21 13:35:27 +0200

    moved folders: APP, __arc__, external tools, test to miniPolyApp repo

--- test/Servo_GL_minion_test.py
+++
@@ -1,7 +0,0 @@
-from APP.GL_test.GL_test_app import GLapp
-import os
-
-if __name__ == "__main__":
-    os.chdir('APP/GL_test')
-    app = GLapp("main")
-    app.run()
--- test/copyfile_test.py
+++
@@ -1,6 +0,0 @@
-import os, shutil
-
-local_dir = '/APP/CaImg_App/data/20230718_113146'
-remote_dir = '\\\\nas3\datastore_bonhoeffer_group$\Yue Zhang\CaData'
-
-shutil.copytree(local_dir, os.path.join(remote_dir, os.path.basename(local_dir)))
